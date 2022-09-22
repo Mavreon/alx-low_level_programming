9316540c37323fa28a74895ac84dd285437782d3
Chisom Ogbonna <61495173+Mavreon@users.noreply.github.com>
2022-09-22 17:44:06 +0100

    Updated script

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,13 +10,24 @@
  */
 char *strcat(char *dest, const char *src)
 {
-	int index = 0, dest_len = 0;
+	// int index = 0, dest_len = 0;
 
-	while (dest[index++])
-		dest_len++;
+	// while (dest[index++])
+	// 	dest_len++;
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	// for (index = 0; src[index]; index++)
+	// 	dest[dest_len++] = src[index];
 
-	return (dest);
+	// return (dest);
+	char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = _strcat(s1, s2);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", ptr);
+
+    return (0);
 }
